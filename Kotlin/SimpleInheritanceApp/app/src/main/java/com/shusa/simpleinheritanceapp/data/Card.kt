f08d5c3f16712b597d96b620bf971e31294ca05f
Tyler Shelter <52799480+SheltortoiseUSA@users.noreply.github.com>
2022-12-15 22:53:04 -0800

    [SIA] Enhancement, Added Card and Card Item

--- Kotlin/SimpleInheritanceApp/app/src/main/java/com/shusa/simpleinheritanceapp/data/Card.kt
+++ Kotlin/SimpleInheritanceApp/app/src/main/java/com/shusa/simpleinheritanceapp/data/Card.kt
@@ -1,4 +1,18 @@
 package com.shusa.simpleinheritanceapp.data
 
-class Card(_name: String?, _shares) {
+import android.util.Log
+import com.shusa.simpleinheritanceapp.R
+
+class Card(_name: String?, _shares: Int?, _inheritance: Double?) {
+    private var name: String? = _name
+    private var shares: Int? = _shares
+    private var inheritance: Double? = _inheritance
+
+    init {
+        Log.d(GlobalContext.getCurrentContext().getString(R.string.log_initialized), javaClass.name)
+    }
+
+    fun getName(): String = name as String
+    fun getShares(): Int = shares as Int
+    fun getInheritance(): Double = inheritance as Double
 }
